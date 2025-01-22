7d06d3cb8aba57b0bfdab5dce60940781dcf8901
jsdodiya <jinendradodiya9@gmail.com>
2025-01-22 10:52:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Update the path as per your project structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
